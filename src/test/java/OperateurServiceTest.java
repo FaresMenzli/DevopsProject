2bf4a96a89645b2ade61b74ad34baffb8845c6e8
Hend <hend.sallemi@esprit.tn>
2023-03-10 21:07:36 +0100

    Operateur test

--- src/test/java/OperateurServiceTest.java
+++ src/test/java/OperateurServiceTest.java
@@ -1,4 +1,47 @@
+import java.util.List;
+import java.util.Optional;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+import tn.esprit.rh.achat.services.OperateurServiceImpl;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@Slf4j
 public class OperateurServiceTest {
     // comment for test
+
+    @Mock
+    OperateurRepository operateurRepository;
+
+    @InjectMocks
+    OperateurServiceImpl operateurService;
+
+    Operateur operateur = new Operateur((long) 1472536, "aa","bbb","azer");
+  
     
+    
+    /**
+     * 
+     */
+    @Test
+    public void testgetOperateur(){
+        Mockito.when(operateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(operateur));
+        Operateur o =operateurService.retrieveOperateur(1L);
+        Assertions.assertNotNull(o);
+    
+    }
+   
 }
